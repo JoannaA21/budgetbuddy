11d9a650e71e386a6fa7e864ab4f6c2436a944dc
Joanna <j.apellido601@mybvc.ca>
2024-03-22 14:59:23 -0600

    LoginPage - user information saved to internal storage and redirected to account.
    ProfilePage - get user information from internal storage and converted text file to JSON.

--- app/src/main/java/com/example/androidproject/loginPage.java
+++ app/src/main/java/com/example/androidproject/loginPage.java
@@ -1,14 +1,16 @@
 package com.example.androidproject;
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
+
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -17,6 +19,8 @@ import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -39,26 +43,28 @@ public class loginPage extends AppCompatActivity {
 
         editTextEmail = findViewById(R.id.email);
         editTextPassword = findViewById(R.id.password);
+
+        Button loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Execute the PostRequestAsyncTask when the button is clicked
+                new ProfileRequestAsyncTask().execute("http://143.198.237.154:3001/api/login");
+            }
+        });
+
     }
 
-    public void loginbtn(View view) {
-        new FetchData().execute("http://143.198.237.154/api/login");
-//        Intent intent = new Intent(this, profilePage.class);
-//        startActivity(intent);
-    }
 
-
-    private class FetchData extends AsyncTask<String, Void, String> {
-        String email =  editTextEmail.getText().toString();
-        String password =  editTextPassword.getText().toString();
-
+    protected class ProfileRequestAsyncTask extends AsyncTask<String, Void, String> {
+        String email = editTextEmail.getText().toString();
+        String password = editTextPassword.getText().toString();
         JSONObject userlogin = new JSONObject();
-
-
 
         @Override
         protected String doInBackground(String... urls) {
             HttpURLConnection urlConnection = null;
+            String response = null;
 
             try {
                 userlogin.put("email", email);
@@ -69,54 +75,92 @@ public class loginPage extends AppCompatActivity {
             }
 
             try {
-                URL url = new URL(urls[0]);
-                urlConnection = (HttpsURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("POST");
-                urlConnection.connect();
+                // Create a HttpURLConnection object and set it up for a POST request
+                URL url = new URL(urls[0]); // params[0] should be the URL
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("POST");
+                connection.setDoOutput(true); // Allow writing data to the connection
 
+                // Set any headers required by the server
+                connection.setRequestProperty("Content-Type", "application/json");
+                connection.setRequestProperty("Accept", "application/json");
 
-                urlConnection.setRequestProperty("Content-Type", "application/json"); //set content type
-                urlConnection.setRequestProperty("Accept", "application/json"); //only accepts json
+                // Convert JSON data to a string
+                String jsonString = userlogin.toString();
+                Log.d("PostRequestAsyncTask", "Request jsonString: " + jsonString);
 
-                urlConnection.setDoOutput(true); // true if you intend to use the URL connection for output
+                // Write the data to be sent in the request body
+                DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
+                outputStream.writeBytes(jsonString);
+                outputStream.flush();
+                outputStream.close();
 
-                //send request to API
-                try(DataOutputStream dataOutputStream = new DataOutputStream(urlConnection.getOutputStream())){
-                    dataOutputStream.writeBytes(userlogin.toString());
-                }
-
-                //response from API
-                BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-                    String  line;
-                    while((line = br.readLine()) != null) {
-                        Log.e("response: ", line);
-                        System.out.println(line);
+                // Get the response from the server and read it
+                int responseCode = connection.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    StringBuilder responseBuilder = new StringBuilder();
+                    String inputLine;
+                    while ((inputLine = in.readLine()) != null) {
+                        responseBuilder.append(inputLine);
                     }
-//                OutputStream outputStream = new BufferedOutputStream(urlConnection.getOutputStream()); //output stream handles data flowing out of a program
-//                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));//writes contents of outputStream
-//                writer.write(userlogin.toString()); //data
-//
-//
-//                writer.flush(); //scans writer(which contains data)
-//                writer.close();
-//                outputStream.close();
-//                Log.e("response: ", urlConnection.getResponseMessage());
-//                urlConnection.disconnect();
-
-
-            } catch(Exception e) {
-                try {
-                    throw new Exception(e);
-                } catch (Exception ex) {
-                    throw new RuntimeException(ex);
+                    in.close();
+                    response = responseBuilder.toString();
+                } else {
+                    response = "POST request failed with response code: " + responseCode;
                 }
+
+                // Close the connection
+                connection.disconnect();
+            } catch (IOException e) {
+                e.printStackTrace();
+                response = "Exception: " + e.getMessage();
             }
+            return response;
+        }
 
-            return null;
+        @Override
+        protected void onPostExecute(String result) {
+            // Handle the response here
+            if (result != null) {
+                // Response received successfully, save info to Internal storage
+                // For example, you can parse the JSON response and update UI components
+                try {
+                    JSONObject jsonResponse = new JSONObject(result); //result in JSON
+                    String message = jsonResponse.optString("message"); //result in String
+                    writeToInternalStorage("credential.txt", result); //Save result in String to the internal storage
 
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    // Handle JSON parsing error
+                }
+
+                Log.d("PostRequestAsyncTask", "Response: " + result);
+            } else {
+                // Handle error case, e.g., API request failed
+                Log.d("PostRequestAsyncTask", "Response: " + result);
+            }
+        }
+
+        public void writeToInternalStorage(String fileName, String content) {
+            File path = getApplicationContext().getFilesDir();
+
+            try {
+                FileOutputStream writer = new FileOutputStream(new File(path, fileName));
+                writer.write(content.getBytes());
+                Log.d("Internal Storage", "Credentials: " + content);
+                redirectWhenSuccessfulLogin();
+            }catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
+
+    public void redirectWhenSuccessfulLogin() {
+        Intent intent = new Intent(this, profilePage.class);     //Change this to dashboard
+        startActivity(intent);
+    }
     public void CreateAccount(View view) {
         Intent intent = new Intent(this, registerPage.class);
         startActivity(intent);
--- app/src/main/java/com/example/androidproject/profilePage.java
+++ app/src/main/java/com/example/androidproject/profilePage.java
@@ -1,14 +1,78 @@
 package com.example.androidproject;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class profilePage extends AppCompatActivity {
+//    public class Profile {
+//        private String name;
+//        private String id;
+//        private String email;
+//    }
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
 
+        try {
 
+            JSONObject credentialIS = getInternalStorage("credential.txt");
+            JSONObject retrieve = credentialIS.getJSONObject("details");
+
+
+            Log.d("creds", "Creds id: " + retrieve.getString("id") +" " + retrieve.getString("email"));
+        } catch(JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+
+//        Profile details = gson.fromJson(credentialIS, Profile.class);
+//        String id = details.id;
+//        Log.d("creds", "Creds id: " + id );
     }
+
+
+    //Get Internal Storage from App
+    public JSONObject getInternalStorage(String fileName) {
+        File path = getApplicationContext().getFilesDir();
+        File readFrom = new File(path, fileName);
+        byte[] content = new byte[(int) readFrom.length()];
+
+        try {
+            FileInputStream stream = new FileInputStream(readFrom);
+            stream.read(content);
+            String json = new String(content);
+            return new JSONObject(json);
+        } catch (IOException | JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+//    public String getInternalStorage(String fileName) {
+//        File path = getApplicationContext().getFilesDir();
+//        File readFrom = new File(path, fileName);
+//        byte[] content = new byte[(int)readFrom.length()];
+//
+//        try{
+//            FileInputStream stream = new FileInputStream(readFrom);
+//            stream.read(content);
+//            String json = new Gson().toJson(new String (content)); //Credentials.txt to JSON
+//            return json;
+//        } catch(IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+
+
+
 }
