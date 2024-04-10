b986dea1d3b8989f558ab2769d96ee28bb207e54
fmagbitang <f.magbitang867@mybvc.ca>
2024-04-09 18:27:35 -0600

    this should fix the updating while adding expense

--- controller/expenseController.js
+++ controller/expenseController.js
@@ -1,6 +1,6 @@
 const Expense = require('../models/expense');
 const { Sequelize } = require('sequelize');
-
+const Income = require('../models/income');
 
 const timeElapsed = Date.now();
 const today = new Date(timeElapsed);
@@ -8,7 +8,7 @@ const today = new Date(timeElapsed);
 // Create expense (post)
 const createExpense = async(req, res, next) => {
     const {expense_type, cost, user_id} = req.body;
-
+    var newCost = 0;
     try {
         created_at = today.toISOString();
         updated_at = today.toISOString();
@@ -20,6 +20,46 @@ const createExpense = async(req, res, next) => {
             created_at,
             updated_at
         });
+
+        const myExpenses = await Expense.findAll(
+            {
+              where: {
+                user_id: user_id,
+              }
+            });
+            myExpenses.forEach(expense => {
+              console.log(expense.cost);
+              newCost += expense.cost;
+            });
+
+        const [incomeGoal, created] = await Income.findOrCreate({
+            where: { user_id: user_id }, // Conditions to find the record
+            defaults: { // Data to be used if no matching record is found
+                user_id: user_id,
+                monthly_income: 0,
+                all_savings: 0,
+                current_balance: 0,
+                expenses: newCost,
+                created_at: today.toISOString(),
+                updated_at: today.toISOString()
+            }
+        });
+        
+        // If the record was found and updated, log the message
+        if (!created) {
+            console.log('Existing record updated:', incomeGoal);
+            // Update the value of the current_balance field in the incomeGoal object
+            console.log(incomeGoal.monthly_income);
+            console.log(incomeGoal.all_savings);
+            // incomeGoal.monthly_income = parseFloat(monthly_income);
+            incomeGoal.expenses = newCost;
+            incomeGoal.current_balance = parseFloat(incomeGoal.monthly_income -  newCost) -  parseFloat(incomeGoal.all_savings);
+  
+            // Save the changes to the database
+            await incomeGoal.save();
+        } else {
+            console.log('New record created:', incomeGoal);
+        }      
 
         res.status(201).json(expense);
     } catch (err) {
